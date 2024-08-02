351de5f7ed91f0f61df100cd7f7915c817a647fc
siddhrajcrest <92163685+siddhrajcrest@users.noreply.github.com>
2024-08-02 13:14:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ fetch(
   res.text().then((d) => {
     const files = JSON.parse(d);
     const mainChunk = document.createElement("script");
-    mainChunk.src = `https://cdn.jsdelivr.net/gh/FunnelAI/Chat_widget_build/build${files.files["main.js"]}`;
+    mainChunk.src = `https://cdn.jsdelivr.net/gh/FunnelAI/Chat_widget_build@staging/build${files.files["main.js"]}`;
     document.head.appendChild(mainChunk);
     const mainChunkCss = document.createElement("link");
-    mainChunkCss.href = `https://cdn.jsdelivr.net/gh/FunnelAI/Chat_widget_build/build${files.files["main.css"]}`;
+    mainChunkCss.href = `https://cdn.jsdelivr.net/gh/FunnelAI/Chat_widget_build@staging/build${files.files["main.css"]}`;
     mainChunkCss.rel = "stylesheet";
     document.head.appendChild(mainChunkCss);
     const react = document.createElement("script");
